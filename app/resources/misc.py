4317ac47d5e3d6bd274983c046468129f6f0cc46
thullyDev <thully.developer@gmail.com>
2024-06-29 16:35:47 +0200

    made the pagination strings

--- app/resources/misc.py
+++ app/resources/misc.py
@@ -1,12 +1,13 @@
 from typing import List
+import math
 
 def paginate_items(data: List, page: int, limit: int):
 	start = (page - 1) * limit
 	end = start + limit
 	items = data[start:end]
 	pagination = {
-			"pages": math.ceil(len(data) / limit),
-			"page": page,
+			"pages": str(math.ceil(len(data) / limit)),
+			"page": str(page),
 	}
 	
 	return items, pagination 
